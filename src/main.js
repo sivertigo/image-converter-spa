2aae54ce67a88f83079a468956f970a0ac7fcc64
sugawara <sg.tak22@gmail.com>
2021-01-30 21:20:29 +0900

    add amplify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import Amplify from 'aws-amplify'
+import awsExports from './aws-exports'
+
 
 Vue.config.productionTip = false
-
+Amplify.configure(awsExports)
+Vue.use(Amplify)
 new Vue({
   vuetify,
   render: h => h(App)
